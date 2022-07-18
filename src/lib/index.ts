fd2d22447dcd8f2cce0839ff120fd3506a345624
Ait0ne666 <yukhanov@lilekov-studio.com>
2022-07-18 16:46:50 +0300

    version

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,9 +5,4 @@ import release from './commands/release'
 import {LoggerConsoleOutput} from '@darksun/logger-console-output'
 log.registerOutput(new LoggerConsoleOutput());
 
-
-
-
-
-
 export {bump, release}
